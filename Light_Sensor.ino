6b03d9af9475466b4fd24b3c37828f53acbc5c37
JohnAPA <40034674+JohnAPA@users.noreply.github.com>
2019-10-01 21:38:13 -0400

    Create Light_Sensor.ino

---
+++ Light_Sensor.ino
@@ -0,0 +1,26 @@
+int sensorPin = 34; //needs to be changed or it will conflict with the pH Probe
+float rawRange = 4096;
+float logRange = 5.0;
+ 
+void setup()
+{
+Serial.begin(115200);
+}
+ 
+void loop()
+{
+// read the raw value from the sensor:
+int rawValue = analogRead(sensorPin);
+ 
+Serial.print("Raw = ");
+Serial.print(rawValue);
+Serial.print(" - Lux = ");
+Serial.println(RawToLux(rawValue));
+delay(1000);
+}
+ 
+float RawToLux(int raw)
+{
+float logLux = raw * logRange / rawRange;
+return pow(10, logLux);
+}
